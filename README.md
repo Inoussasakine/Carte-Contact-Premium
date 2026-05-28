2392baa267343d7cdf9090dd1ee099839fffdc95
Inoussasakine <inoussasakine@gmail.com>
2026-05-28 20:02:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Carte Contact Premium
+par Sakine inoussa
+mail: inoussasakine@gmail.com
+github : @Inoussasakine
+
+## Fonctionnalités
+- Carte de profil premium moderne
+- Compatible Android & iPhone
+- Bouton Ajouter aux contacts
+- Téléchargement automatique .VCF
+- Boutons rapides :
+  - Appel
+  - WhatsApp
+  - Email
+  - Site web
+- Responsive mobile
+
+## Fichiers
+- index.html
+- style.css
+- script.js
+
+## Utilisation
+Ouvrez simplement index.html dans un navigateur ou l'intégré minutieusement dans son projet existant.
+
+## Hébergement conseillé
+- Netlify
+- Vercel
+- GitHub Pages
+- Hostinger
---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+
+document.getElementById("downloadContact").addEventListener("click", function(){
+
+const vcard = `
+BEGIN:VCARD
+VERSION:3.0
+FN:Roseline SEDDOH NASSARAH
+N:SEDDOH NASSARAH;Roseline;;;
+ORG:Spa Fleur d'Ebène | Nina Style
+TITLE:CEO
+TEL;TYPE=CELL:+2290196444879
+EMAIL:spa.cotonou@spafleurdebene.com
+URL:https://spafleurdebene.com
+ADR:;;Cotonou;;;Benin
+NOTE:Directrice Génerale des Spas Fleur d'Ebène de Cotonou, Lomé, Abidjan, Bamako, Niamey, Ouagadougou.
+END:VCARD
+`;
+
+const blob = new Blob([vcard], { type: 'text/vcard' });
+
+const link = document.createElement('a');
+
+link.href = URL.createObjectURL(blob);
+link.download = 'Roseline_Seddoh_Nassarah.vcf';
+
+document.body.appendChild(link);
+
+link.click();
+
+document.body.removeChild(link);
+
+});
