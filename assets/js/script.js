b4c8763621ed222668eef84dd9a19187390457fe
EmmanuelKP <ekpotin@gmail.com>
2026-05-30 10:09:40 +0100

    MAJ Titre Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 
 document.getElementById("downloadContact").addEventListener("click", function(){
 
-const vcard = `
-BEGIN:VCARD
+const vcard = `BEGIN:VCARD
 VERSION:3.0
 FN:Roseline SEDDOH NASSARAH
 N:SEDDOH NASSARAH;Roseline;;;
@@ -13,20 +12,20 @@ EMAIL:spa.cotonou@spafleurdebene.com
 URL:https://spafleurdebene.com
 ADR:;;Cotonou;;;Benin
 NOTE:Directrice Génerale des Spas Fleur d'Ebène de Cotonou, Lomé, Abidjan, Bamako, Niamey, Ouagadougou.
-END:VCARD
-`;
+END:VCARD`;
 
-const blob = new Blob(["﻿" + vcard], { type: 'text/vcard;charset=utf-8' });
+const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-const link = document.createElement('a');
-
-link.href = URL.createObjectURL(blob);
-link.download = 'Roseline_Seddoh_Nassarah.vcf';
-
-document.body.appendChild(link);
-
-link.click();
-
-document.body.removeChild(link);
+if (isIOS) {
+  window.location.href = 'data:text/vcard;charset=utf-8,' + encodeURIComponent(vcard);
+} else {
+  const blob = new Blob(["﻿" + vcard], { type: 'text/vcard;charset=utf-8' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = 'Roseline_Seddoh_Nassarah.vcf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
 
 });
