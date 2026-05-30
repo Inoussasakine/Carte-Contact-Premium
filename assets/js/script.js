a04a996d7de96934bbd26656c08eeb1a949df8d7
Inoussasakine <inoussasakine@gmail.com>
2026-05-30 12:42:00 +0100

    vcf 2nd methode commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,35 @@
 
-document.getElementById("downloadContact").addEventListener("click", function(){
-
-const vcard = `
-BEGIN:VCARD
-VERSION:3.0
-FN:Roseline SEDDOH NASSARAH
-N:SEDDOH NASSARAH;Roseline;;;
-ORG:Spa Fleur d'Ebène | Nina Style
-TITLE:CEO
-TEL;TYPE=CELL:+2290196444879
-EMAIL:spa.cotonou@spafleurdebene.com
-URL:https://spafleurdebene.com
-ADR:;;Cotonou;;;Bénin
-NOTE:Directrice Génerale des Spas Fleur d'Ebène de Cotonou, Lomé, Abidjan, Bamako, Niamey, Ouagadougou.
-END:VCARD
-`;
-
-const blob = new Blob(["﻿" + vcard], { type: 'text/vcard;charset=utf-8' });
-
-const link = document.createElement('a');
-
-link.href = URL.createObjectURL(blob);
-link.download = 'Roseline_Seddoh_Nassarah.vcf';
-
-document.body.appendChild(link);
-
-link.click();
-
-document.body.removeChild(link);
-
+document.getElementById('downloadContact2').addEventListener('click', () => {
+    window.location.href = './assets/img/Roseline_Seddoh_Nassarah_ceo.vcf';
 });
+// document.getElementById("downloadContact").addEventListener("click", function(){
+
+// const vcard = `
+// BEGIN:VCARD
+// VERSION:3.0
+// FN:Roseline SEDDOH NASSARAH
+// N:SEDDOH NASSARAH;Roseline;;;
+// ORG:Spa Fleur d'Ebène | Nina Style
+// TITLE:CEO
+// TEL;TYPE=CELL:+2290196444879
+// EMAIL:spa.cotonou@spafleurdebene.com
+// URL:https://spafleurdebene.com
+// ADR:;;Cotonou;;;Bénin
+// NOTE:Directrice Génerale des Spas Fleur d'Ebène de Cotonou, Lomé, Abidjan, Bamako, Niamey, Ouagadougou.
+// END:VCARD
+// `;
+
+// const blob = new Blob(["﻿" + vcard], { type: 'text/vcard;charset=utf-8' });
+
+// const link = document.createElement('a');
+
+// link.href = URL.createObjectURL(blob);
+// link.download = 'Roseline_Seddoh_Nassarah.vcf';
+
+// document.body.appendChild(link);
+
+// link.click();
+
+// document.body.removeChild(link);
+
+// });
